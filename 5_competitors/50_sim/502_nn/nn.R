9be4e2d1ddca6aa3a07a1e66e7e9bf06ef8214db
Ethan4444444 <ethan.pawl.22@gmail.com>
2026-07-16 22:58:30 -0700

    ml model planning

---
+++ 5_competitors/50_sim/502_nn/nn.R
@@ -0,0 +1,10 @@
+# TODO: try
+
+# randomForestSRC # multivariate regression forests
+# multivariateRandomForest  # multivariate regression forests, maybe not hugely maintained
+# mboost # multivariate distributional boosting (statistical)
+# mvtboost	(covariance modeling? but old and may be troublesome)
+# xgboost or ranger or lightgbm (one forest per output dimension)
+# torch (multivariate output NN)
+
+# TODO: start with multivariate random forest
