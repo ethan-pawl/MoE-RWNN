8ea65bc6fd8cb4167cd609d57e04767aec459e5a
Ethan4444444 <ethan.pawl.22@gmail.com>
2026-09-25 05:58:09 -0700

    simplify code for weights experiment

--- 2_application/22_wts/0_wts.R
+++ 2_application/22_wts/0_wts.R
@@ -1,50 +1,42 @@
 library(flowmix)
-library(parallel)
-library(gtools)
-library(dplyr)
-library(tidyr)
-library(tibble)
-library(reshape2)
+
+# Load the data
+datobj <- readRDS(file = file.path("data", "MGL1704-hourly-paper-counts-and-biomass.RDS"))
+
+ylist <- datobj$ybin_list 
+countslist <- datobj$countslist
+
+X_dir <- file.path(
+  "data", 
+  "X_variations"
+)
+
+X <- readRDS(file.path(X_dir, "X_pc_9_nh_70_seed_4_ofold_NA_ifold_NA.RDS"))
+
+load(file.path("2_application", "22_wts", "settings.Rdata"))
+
+nl_no_wts_best <- flowmix_once(
+  ylist = ylist, 
+  countslist = countslist, 
+  X = X, 
+  prob_lambda = prob_lambda, 
+  mean_lambda = mean_lambda, 
+  maxdev = 0.5, 
+  numclust = 10, 
+  seed = seed, 
+  verbose = TRUE
+)
+
+load(file.path("2_application", "1_all_data", "nl_fit.Rdata"))
+nl_best <- res
+
+library(flowtrend)
 library(ggplot2)
 library(ggpubr)
-library(grid)
 library(gridExtra)
-library(maps)
-library(RColorBrewer)
-
-plots_dir <- "plots"
-if(!dir.exists(plots_dir)) dir.create(plots_dir)
-
-# Load all data
-datobj1 <- readRDS(file = file.path("data", "MGL1704-hourly-paper-counts-and-biomass.RDS"))
-
-# Collect results
-nl_cv_file <- file.path(
-  "2_application", 
-  "21_all_data", 
-  "results", 
-  "nl_9_70_4", 
-  "nl_9_70_4_pcr_summary.RDS"
-)
-
-nl_no_wts_file <- file.path(
-  "2_application", 
-  "22_wts", 
-  "results", 
-  "nl_no_wts_9_70_4", 
-  "nl_no_wts_9_70_4_wts_summary.RDS"
-)
-
-nl_cv <- readRDS(nl_cv_file)
-nl_cv_no_wts <- readRDS(nl_no_wts_file)
-
-nl_best <- nl_cv$bestres
-nl_no_wts_best <- nl_cv_no_wts$bestres
-
-##########################
 
 plot_list_nl <- flowtrend::plot_3d(
-  datobj1$ybin_list, nl_best, 33, datobj1$biomass_list, return_list_of_plots = TRUE, 
+  datobj$ybin_list, nl_best, 33, datobj$biomass_list, return_list_of_plots = TRUE, 
   labels = c(
     "Pico2", "Pico1", "Other5", "Bead", "Other1", "Syn", "Other6", "Other2", "Pro", "Other4"
   ), 
@@ -55,7 +47,7 @@ plot_list_nl <- flowtrend::plot_3d(
 p1 <- plot_list_nl[[1]]
 
 plot_list_nl_no_wts <- flowtrend::plot_3d( # Still plot the biomass even though the model doesn't see it
-  datobj1$ybin_list, nl_no_wts_best, 33, datobj1$biomass_list, return_list_of_plots = TRUE, 
+  datobj$ybin_list, nl_no_wts_best, 33, datobj$biomass_list, return_list_of_plots = TRUE, 
   labels = c(
     "Syn", "Other8", "Pico", "Pro1", "Other7", "Other1", "Pro6", "Pro5", "Pro2", "Pro3"
   ), 
@@ -85,7 +77,7 @@ x_lab <- text_grob("Diameter", size = 12, x = 0.515, y = 1)
 overall_title <- text_grob("Estimated Models\n2017-07-02 08:00-09:00", 
                            size = 18)
 
-pdf(file.path(plots_dir, "Figure-wts-comp.pdf"), 10.5, 6)
+pdf(file.path("plots", "SuppFigure14.pdf"), 10.5, 6)
 print(grid.arrange(p1, p2, x_lab, overall_title, layout_matrix = matrix(c(4, 4, 
                                                                     1, 2, 
                                                                     3, 3), 
